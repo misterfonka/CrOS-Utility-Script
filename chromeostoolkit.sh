d0c32c75d7c8218f06af3443db3319fb4cc7d0ba
misterfonka <127918822+misterfonka@users.noreply.github.com>
2023-06-20 00:14:30 -0500

    Update chromeostoolkit.sh

--- chromeostoolkit.sh
+++ chromeostoolkit.sh
@@ -60,16 +60,106 @@ SetGBB () {
   # Removes bios.bin file from current directory
   rm bios.bin
 }
+BIOSDump () {
+    # Creates bios.bin file in current directory
+    flashrom -r bios.bin &> /dev/null
+}
+# Function to print section headers
+print_header() {
+    local header_text=$1
+    echo -e "\n\e[1m$header_text\e[0m"
+}
+
+# Function to get CPU usage
+get_cpu_usage() {
+    cpu_usage=$(top -bn1 | grep "Cpu(s)" | awk '{print $2 + $4}')
+    echo "$cpu_usage%"
+}
+
+# Function to get memory usage
+get_memory_usage() {
+    memory_usage=$(free -m | awk 'NR==2{printf "Used: %sMB (%.2f%%)\n", $3,$3*100/$2 }')
+    echo "$memory_usage"
+}
+
+# Function to get disk space
+get_disk_space() {
+    disk_space=$(df -h | awk '$NF=="/"{printf "Total: %sB\nUsed: %sB (%s)\nFree: %sB (%s)\n", $2,$3,$5,$4,$6}')
+    echo "$disk_space"
+}
+
+# Function to get network statistics
+get_network_statistics() {
+    network_interface=$(ip -o -4 route show to default | awk '{print $5}')
+    network_stats=$(ip -o -4 addr show dev $network_interface | awk '{print "IP: "$4}')
+    network_stats+="\n$(ip link show dev $network_interface | awk '/ether/ {print "MAC: "$2}')"
+    echo -e "$network_stats"
+}
+
+# Function to get load average
+get_load_average() {
+    load_average=$(uptime | awk -F 'load average:' '{print $2}')
+    echo "$load_average"
+}
+
+# Function to get CPU temperature
+get_cpu_temperature() {
+    cpu_temperature=$(sensors | awk '/^Core/ {print "Core "++i": "$3}')
+    echo "$cpu_temperature"
+}
+
+# Function to get logged-in users
+get_logged_in_users() {
+    logged_in_users=$(who | awk '{print $1}')
+    echo "$logged_in_users"
+}
+
+# Function to get process count
+get_process_count() {
+    process_count=$(ps -e -o user= | sort | uniq -c | awk '{print $2": "$1}')
+    echo "$process_count"
+}
+
+# Function to get system uptime
+get_system_uptime() {
+    system_uptime=$(uptime -p)
+    echo "$system_uptime"
+}
+
+# Function to get swap usage
+get_swap_usage() {
+    swap_total=$(free -m | awk 'NR==3{print $2}')
+    swap_used=$(free -m | awk 'NR==3{print $3}')
+    
+    if [[ $swap_total -gt 0 ]]; then
+        swap_usage=$(awk -v used="$swap_used" -v total="$swap_total" 'BEGIN { printf "Used: %dMB (%.2f%%)\n", used, (used / total) * 100 }')
+    else
+        swap_usage="No swap space available."
+    fi
+    
+    echo "$swap_usage"
+}
+FWWPStatus() {
+    fwwp_status=$(crossystem wpsw_cur)
+    if [[ $wp_status = "0" ]]; then
+        echo "Disabled"
+    else
+        echo "Enabled"
+}
 
 clear
-echo    "Menu Options:"
-echo    "*************************************"
+echo -e "\e[1mWelcome to my menu!\e[0m"
+echo -e "\e[1mSome extra information:\e[0m"
+echo    "FW WP: " FWWPStatus
+echo -e "\e[1m*************************************\e[0m"
 echo    "1) Disable autoupdates"
 echo    "2) Remove rootfs verification"
 echo    "3) Edit VPD"
 echo    "4) Edit GBB flags"
 echo    "5) Mac Address Randomizer"
 echo    "6) Run MrChromeboxes Firmware Utility"
+echo    "7) Dump BIOS/Firmware"
+echo    "8) System Info"
 read -p "Select the number corresponding to what you want to do: " user_choice
 
 if [[ "$user_choice" = "1" ]]; then
@@ -178,6 +268,7 @@ elif [[ "$user_choice" = "4" ]]; then
     read -p "What do you want to set the GBB flags to? " set_gbbchoice
     clear
     echo "Are you sure you want to set your GBB flags to $set_gbbchoice?"
+    echo "Note: Firmware WP needs to be disabled."
     read -p "Type DoIt to confirm: " doit
 
     if [[ "$doit" = "DoIt" ]]; then
@@ -277,6 +368,60 @@ elif [[ "$user_choice" = "6" ]]; then
    curl -LOk mrchromebox.tech/firmware-util.sh
    clear
    sudo bash firmware-util.sh
-else
-    echo "ERROR: Invalid option"
+
+elif [[ "$user_choice" = "7" ]]; then
+    clear
+    echo "Are you sure you want to dump the current system BIOS/Firmware?"
+    echo "Nothing bad can come from this, it will just make the bios.bin file"
+    echo "in the directory you ran this script in."
+    echo ""
+    read -p "Type DoIt to confirm: " doit
+
+    if [[ "$doit" = "DoIt" ]]; then
+        clear
+        echo "Dumping system BIOS/Firmware..."
+        BIOSDump
+        echo "Dumped successfully."
+    else
+        clear
+        echo "User didn't want to do it..."
+    fi
+
+elif [[ "$user_choice" = "8" ]]; then
+    # Customize the script's output appearance
+    clear
+    echo -e "\e[1mSystem Information\e[0m"
+    echo "---------------------"
+
+    # Gather and display system information
+    print_header "General"
+    get_cpu_usage
+    get_memory_usage
+    get_disk_space
+
+    print_header "Network"
+    get_network_statistics
+
+    print_header "System Load"
+    get_load_average
+
+    print_header "CPU Temperature"
+    get_cpu_temperature
+
+    print_header "Logged-in Users"
+    get_logged_in_users
+
+    print_header "Process Count"
+    get_process_count
+
+    print_header "System Uptime"
+    get_system_uptime
+
+    print_header "Swap Usage"
+    get_swap_usage
+
+    echo
+
+    # Press anykey to continue
+    read -p "Press anykey to continue: "
 fi
