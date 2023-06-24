ad1510715ee545deb09e0803401c6f8b302b4fc3
misterfonka <127918822+misterfonka@users.noreply.github.com>
2023-06-24 18:42:25 -0500

    Update chromeostoolkit.sh

--- chromeostoolkit.sh
+++ chromeostoolkit.sh
@@ -1,240 +1,12 @@
 #!/bin/bash
 
 # Author: misterfonka
-# Purpose: Toolkit for ChromeOS that comes with cool stuff kind of just something random I wanted to make lol. I will be adding more.
+# Purpose: crOS Utility Script filled with useful utilities for ChromeOS.
 
-# Variables
+# Sources the functions from the other file
+source functions.sh
 
-HWID=$(crossystem hwid | sed 's/X86//g' | sed 's/ *$//g' | sed 's/ /_/g')
-BOARD=$(crossystem hwid | sed 's/X86//g' | sed 's/ *$//g'| awk 'NR==1{print $1}' | cut -f 1 -d'-')
-FWVERSION=$(crossystem fwid)
-
-# Functions
-echo_red() {
-	echo -e "\E[0;31m$1"
-	echo -e '\e[0m'
-}
-echo_green() {
-	echo -e "\E[0;32m$1\e[0m"
-}
-echo_yellow() {
-	echo -e "\E[1;33m$1\e[0m"
-}
-echo_blue() {
-    echo -e "\033[34m$1\033[0m"
-}
-CheckEnrollmentRO() {
-    vpd -i RO_VPD -s check_enrollment="$RO_VPD_edited"
-}
-MfgDate() {
-    vpd -i RO_VPD -s mfg_date="$RO_VPD_edited"
-}
-MlbSerialNumber() {
-    vpd -i RO_VPD -s mlb_serial_number="$RO_VPD_edited"
-}
-ModelName() {
-    vpd -i RO_VPD -s model_name="$RO_VPD_edited"
-}
-Region() {
-    vpd -i RO_VPD -s region="$RO_VPD_edited"
-}
-SerialNumber() {
-    vpd -i RO_VPD -s serial_number="$RO_VPD_edited"
-}
-SKUNumber() {
-    vpd -i RO_VPD -s sku_number="$RO_VPD_edited"
-}
-StableDeviceSecret() {
-    vpd -i RO_VPD -s stable_device_secret_DO_NOT_SHARE="$RO_VPD_edited"
-}
-ActivateDate() {
-    vpd -i RW_VPD -s ActivateDate="$RW_VPD_edited"
-}
-BlockDevmode() {
-    vpd -i RW_VPD -s block_devmode="$RW_VPD_edited"
-}
-CheckEnrollmentRW() {
-    vpd -i RW_VPD -s check_enrollment="$RW_VPD_edited"
-}
-FirstActiveOmahaPingSent() {
-    vpd -i RW_VPD -s first_active_omaha_ping_sent="$RW_VPD_edited"
-}
-GBindAttribute() {
-    vpd -i RW_VPD -s gbind_attribute="$RW_VPD_edited"
-}
-ShouldSendRlzPing() {
-    vpd -i RW_VPD -s should_send_rlz_ping="$RW_VPD_edited"
-}
-UBindAttribute() {
-    vpd -i RW_VPD -s ubind_attribute="$RW_VPD_edited"
-}
-SetGBB () {
-  # Creates bios.bin file in current directory
-  flashrom -r bios.bin &> /dev/null
-  # Sets GBB flags to the value the user put in
-  gbb_utility --set --flags=$set_gbbchoice bios.bin &> /dev/null
-  # Writes updated bios.bin file
-  flashrom -i GBB -w bios.bin &> /dev/null
-  # Removes bios.bin file from current directory
-  rm bios.bin
-}
-ViewGBB() {
-	flashrom -r bios.bin &> dev/null
-	view_gbb=$(gbb_utility --get --flags bios.bin | grep -w "flags:" | tr -d "flags :")
-	rm bios.bin
-}
-echo "Getting device/system info..."
-ViewGBB
-BIOSDump () {
-    # Creates bios.bin file in current directory
-    flashrom -r bios.bin &> /dev/null
-}
-# Function to print section headers
-print_header() {
-    local header_text=$1
-    echo -e "\n\e[1m$header_text\e[0m"
-}
-
-# Function to get CPU usage
-get_cpu_usage() {
-    cpu_usage=$(top -bn1 | grep "Cpu(s)" | awk '{print $2 + $4}')
-    echo "$cpu_usage%"
-}
-
-# Function to get memory usage
-get_memory_usage() {
-    memory_usage=$(free -m | awk 'NR==2{printf "Used: %sMB (%.2f%%)\n", $3,$3*100/$2 }')
-    echo "$memory_usage"
-}
-
-# Function to get disk space
-get_disk_space() {
-    disk_space=$(df -h | awk '$NF=="/"{printf "Total: %sB\nUsed: %sB (%s)\nFree: %sB (%s)\n", $2,$3,$5,$4,$6}')
-    echo "$disk_space"
-}
-
-# Function to get network statistics
-get_network_statistics() {
-    network_interface=$(ip -o -4 route show to default | awk '{print $5}')
-    network_stats=$(ip -o -4 addr show dev $network_interface | awk '{print "IP: "$4}')
-    network_stats+="\n$(ip link show dev $network_interface | awk '/ether/ {print "MAC: "$2}')"
-    echo -e "$network_stats"
-}
-
-# Function to get load average
-get_load_average() {
-    load_average=$(uptime | awk -F 'load average:' '{print $2}')
-    echo "$load_average"
-}
-
-# Function to get CPU temperature
-get_cpu_temperature() {
-    cpu_temperature=$(sensors | awk '/^Core/ {print "Core "++i": "$3}')
-    echo "$cpu_temperature"
-}
-
-# Function to get logged-in users
-get_logged_in_users() {
-    logged_in_users=$(who | awk '{print $1}')
-    echo "$logged_in_users"
-}
-
-# Function to get process count
-get_process_count() {
-    process_count=$(ps -e -o user= | sort | uniq -c | awk '{print $2": "$1}')
-    echo "$process_count"
-}
-
-# Function to get system uptime
-get_system_uptime() {
-    system_uptime=$(uptime -p)
-    echo "$system_uptime"
-}
-
-# Function to get swap usage
-get_swap_usage() {
-    swap_total=$(free -m | awk 'NR==3{print $2}')
-    swap_used=$(free -m | awk 'NR==3{print $3}')
-    
-    if [[ $swap_total -gt 0 ]]; then
-        swap_usage=$(awk -v used="$swap_used" -v total="$swap_total" 'BEGIN { printf "Used: %dMB (%.2f%%)\n", used, (used / total) * 100 }')
-    else
-        swap_usage="No swap space available."
-    fi
-    
-    echo "$swap_usage"
-}
-FWWPStatus() {
-    fwwp_status=$(crossystem wpsw_cur)
-    if [[ $fwwp_status == "0" ]]; then
-        echo "Disabled"
-    else
-        echo "Enabled"
-    fi
-}
-edit_crossystem() {
-			clear
-      # no here-strings because of posix sh. sed instead of {//} because posix sh
-      
-      crossystem_rw=$(asusb crossystem | grep "RW")
-      data="$(while read line; do
-        stripped=$(echo "$line" | sed "s/#.*//g" | sed "s/ //g")
-
-        key=$(echo "$stripped" | sed "s/=.*//g")
-        val=$(echo "$stripped" | sed "s/.*=//g")
-
-
-        echo -n "$key\x20(current=$val) "
-      done <<EOF
-$crossystem_rw
-EOF
-)"
-
-        read -p "What do you want to edit?" $data
-
-        line=$(echo "$crossystem_rw" | sed "${CHOICE}q;d")
-        stripped=$(echo "$line" | sed "s/#.*//g" | sed "s/ //g")
-        key=$(echo "$stripped" | sed "s/=.*//g")
-        val=$(echo "$stripped" | sed "s/.*=//g")
-
-
-
-        clear
-        asusb stty echo
-        pick_input "What do you want to change $key to? (current value: $val)          >"
-        clear
-        if crossystem "$key=$CHOICE"; then
-          echo "Set $key to $CHOICE sucessfully"
-        else
-          echo "Failed to set $key to $CHOICE"
-        fi
-}
-reset_system() {
-  clear
-  echo "How would you like to reset system data?"
-  echo  "1) Powerwash (remove user accounts only)"
-  echo  "2) Pressurewash (remove all data)"
-  echo  "3) Secure Wipe (slow, completely unrecoverable)"
-  read -p "Enter the corresponding number you want to do: " CHOICE
-  case $CHOICE in
-  1)
-    mkdir /stateful || :
-    mount "$STATEDEV" /stateful
-    echo "fast safe" >/stateful/factory_install_reset
-    umount /stateful
-    sync
-    ;;
-  2)
-    yes | asusb mkfs.ext4 $STATEDEV
-    ;;
-  3)
-    echo "Starting Secure Wipe..."
-    echo -en "\n\n\n"
-    dd if=/dev/zero | (asusb /usr/sbin/pv) | dd of="$STATEDEV" 
-    echo "Secure Wipe complete"
-    ;;
-  esac
-}
+# Case statement to try and find the users model name
 # Note: this case statement was taken from MrChromeboxes source code.
 case "${HWID}" in
 	AKALI*)                 _x='Acer Chromebook 13 / Spin 13' ; device="nami";;
@@ -538,6 +310,8 @@ case "${HWID}" in
 	ZAVALA*)                _x='Acer Chromebook Vero 712'; device="zavala" ;;
 	*)                      _x='ERROR: unknown or unidentifiable device' ;; 
 esac
+
+# Clear out the previous terminal output and display the menu
 clear
 echo_red "ChromeOS Utility Script"
 echo "misterfonka <misterfonka@gmail.com>"
@@ -554,273 +328,55 @@ echo "$(echo_blue "** [WP]") $(echo_yellow " 2)")  Re-Enable Autoupdates"
 echo "$(echo_blue "** [WP]") $(echo_yellow " 3)")  Disable RootFS verification"
 echo "$(echo_blue "**") $(echo_yellow "      4)")  Edit VPD"
 echo "$(echo_blue "** [WP]" ) $(echo_yellow " 5)")  Edit GBB flags"
-echo "$(echo_blue "**") $(echo_yellow "      6)")  Mac Address Randomizer"
-echo "$(echo_blue "** [WP?]") $(echo_yellow "7)")  Run MrChromeboxes Firmware Utility"
-echo "$(echo_blue "**") $(echo_yellow "      8)")  Dump BIOS/Firmware"
-echo "$(echo_blue "**") $(echo_yellow "      9)")  System Info"
-echo "$(echo_blue "**") $(echo_yellow "      10)") FREDestroyer"
-echo "$(echo_blue "**") $(echo_yellow "      11)") Unblock Devmode"
-echo "$(echo_blue "**") $(echo_yellow "      12)") Edit Crossystem"
-echo "$(echo_blue "**") $(echo_yellow "      13)") Reset System"
+echo "$(echo_blue "**") $(echo_yellow "      6)")  Unblock Devmode"
+echo "$(echo_blue "**") $(echo_yellow "      7)")  FREDestroyer"
+echo "$(echo_blue "**") $(echo_yellow "      8)")  Reset System"
+echo "$(echo_blue "**") $(echo_yellow "      9)") System Info"
+echo "$(echo_blue "** [WP?]") $(echo_yellow "10)") Run MrChromeboxes Firmware Utility"
+echo "$(echo_blue "**") $(echo_yellow "      11)") Mac Address Randomizer"
+echo "$(echo_blue "**") $(echo_yellow "      12)") Dump BIOS/Firmware"
 echo "$(echo_blue "**") $(echo_yellow "      R)")  Reboot"
 echo "$(echo_blue "**") $(echo_yellow "      Q)")  Quit"
 read -p "Select the number corresponding to what you want to do: " user_choice
 
+# Do whatever number the user put in
 if [[ "$user_choice" = "1" ]]; then
-    clear
-    echo "WARNING!"
-    echo "YOU WILL NEED TO DISABLE ROOTFS VERIFICATION TO DO THIS."
-    echo "ALSO, IF YOU DO THIS YOU WILL BE STUCK AT THE CHECKING FOR UPDATES SCREEN"
-    echo "IF YOU POWERWASH. TO FIX THIS, RECOVER. IF YOU WANT TO RE-ENABLE"
-	echo "AUTOUPDATES, USE OPTION 2."
-    echo ""
-    read -p "Type DoIt to continue: " doit
+	disable_autoupdates
 
-    # Remove autoupdates
-    clear
-    if [[ "$doit" = "DoIt" ]]; then
-    echo "Removing autoupdates..."
-    mv /usr/sbin/update_engine /usr/sbin/update_enginefake
-	mv /usr/sbin/update_engine_client /usr/sbin/update_engine_clientfake
-    echo "Succesfully removed autoupdates!"
-
-else
-    clear
-    echo "User didn't want to do it... :("
-fi
+elif [[ "$user_choice" = "2" ]]; then
+	reenableautoupdates
 
 elif [[ "$user_choice" = "3" ]]; then
-    clear
-    echo "TO RESET THE ROOTFS VERIFICATION BACK TO NORMAL, RECOVER."
-    read -p "Type DoIt to continue: " doit
-
-    # Remove RootFS verification
-    clear
-    if [[ "$doit" = "DoIt" ]]; then
-        echo "Removing RootFS verification..."
-        /usr/share/vboot/bin/make_dev_ssd.sh --remove_rootfs_verification --partitions 2
-        echo "Succesfully removed RootFS verification!"
-        echo "Rebooting..."
-        reboot
-    else
-        clear
-        echo "User didn't want to do it... :("
-    fi
+	remove_rootfs_verification
 
 elif [[ "$user_choice" = "4" ]]; then
-clear
-    echo "Choose a VPD partition to edit:"
-    echo "1) Read-Writable (RW_VPD)"
-    echo "2) Write-Protected (RO_VPD)"
-    read -p "Choose one based on the corresponding number: " vpd_choice
+	edit_vpd
 
-        # Choose what to edit in whatever VPD partition they chose
-        clear
-        if [[ "$vpd_choice" = "1" ]]; then
-            echo "What do you want to edit?"
-            echo "ActivateDate  block_devmode   check_enrollment    first_active_omaha_ping_sent    gbind_attribute     should_send_rlz_ping    ubind_attribute"   
-            read -p "Choose the name of what you want to edit: " RW_VPD_editname
-            clear
-            read -p "What do you want to change $RW_VPD_editname to?: " RW_VPD_edited
-            clear
-            echo "Are you sure you want to edit this? This could potentially be dangerous."
-            echo "Name: $RW_VPD_editname"
-            echo "Editing to: $RW_VPD_edited"
-            echo ""
-            read -p "Type DoIt to confirm: " doit
-
-        elif [[ "$vpd_choice" = "2" ]]; then
-            echo "What do you want to edit?"
-            echo "check_enrollment  mfg_date    mlb_serial_number   model_name  region  serial_number   sku_number  stable_device_secret_DO_NOT_SHARE"
-            read -p "Choose the name of what you want to edit: " RO_VPD_editname
-            read -p "What do you want to change $RO_VPD_editname to?: " RO_VPD_edited
-            echo "Are you sure you want to edit this? This could potentially be dangerous."
-            echo "Name: $RO_VPD_editname"
-            echo "Editing to: $RO_VPD_edited"
-            echo ""
-            read -p "Type DoIt to confirm: " doit
-        fi
-
-             if [[ "$doit" = "DoIt" ]]; then
-                echo "Let's do it!"
-            else
-                echo "User didn't want to do it... :("
-                exit 0
-            fi
-
-                    case "$RO_VPD_editname" in
-                    "check_enrollment") CheckEnrollmentRO ;;
-                    "mfg_date") MfgDate ;;
-                    "mlb_serial_number") MlbSerialNumber ;;
-                    "model_name") ModelName ;;
-                    "region") Region ;;
-                    "serial_number") SerialNumber ;;
-                    "sku_number") SKUNumber ;;
-                    "stable_device_secret_DO_NOT_SHARE") StableDeviceSecret ;;
-                    esac
-
-                    case "$RW_VPD_editname" in
-                    "ActivateDate") ActivateDate ;;
-                    "block_devmode") BlockDevmode ;;
-                    "check_enrollment") CheckEnrollmentRW ;;
-                    "first_active_omaha_ping_sent") FirstActiveOmahaPingSent ;;
-                    "gbind_attribute") GBindAttribute ;;
-                    "should_send_rlz_ping") ShouldSendRlzPing ;;
-                    "ubind_attribute") UBindAttribute ;;
-                    esac
-                    
 elif [[ "$user_choice" = "5" ]]; then
-    clear
-    read -p "What do you want to set the GBB flags to? " set_gbbchoice
-    clear
-    echo "Are you sure you want to set your GBB flags to $set_gbbchoice?"
-    echo "Note: Firmware WP needs to be disabled."
-    read -p "Type DoIt to confirm: " doit
-
-    if [[ "$doit" = "DoIt" ]]; then
-        clear
-        echo "Setting GBB flags to $set_gbbchoice..."
-        SetGBB
-        echo "Set GBB flags to $set_gbbchoice successfully."
-    else
-        clear
-        echo "User didn't want to do it..."
-    fi
-
+	edit_gbb
+                    
 elif [[ "$user_choice" = "6" ]]; then
-    clear
-    # NOTE: ALL CREDITS FOR THE MAC ADDRESS RANDOMIZER GO TO MERCURY WORKSHOP.
-    if [ $(id -u) -ne 0 ]; then
-	    echo "Run this script as root (sudo)."
-	    exit 1
-    fi
-
-    echo "------------------------------"
-    echo "Mercury MAC Address Randomizer"
-    echo "This script was made by Mercury"
-    echo "Workshop. Go support them on"
-    echo "Github profile."
-    echo "------------------------------"
-    echo ""
-
-    state=$(cat /sys/class/net/wlan0/operstate)
-
-    mac_dev=$(cat /sys/firmware/vpd/ro/wifi_mac0)
-    echo "Device MAC: ${mac_dev}"
-
-    get_mac_cur() {
-	    cat /sys/class/net/wlan0/address
-    }
-    mac_cur="$(get_mac_cur)"
-    echo "Current MAC: ${mac_cur}"
-
-    gen_mac() {
-	    echo "$(echo "${mac_dev}" | head -c $((-3 * $1 + 17))):$(hexdump -e '1/1 "%02x:"' -v -n $1 /dev/urandom)" | head -c 17
-    }
-
-    echo "Choose an action:"
-    echo "3 - Randomize last 3 bytes."
-    echo "5 - Randomize last 5 bytes."
-    echo "c - Custom MAC address."
-    if [ "${mac_cur}" != "${mac_dev}" ]; then
-	    echo "r - Reset MAC address"
-    fi
-    echo "e - Cancel and exit"
-    read -p "> " action
-
-    case "${action}" in
-	    3)
-		    mac_new="$(gen_mac 3)"
-		    ;;
-	    5)
-		    mac_new="$(gen_mac 5)"
-		    ;;
-	    c | C)
-		    read -p "Enter new MAC address: " mac_new
-		    ;;
-	    r | R)
-		    mac_new="${mac_dev}"
-		    ;;
-	    e | E)
-		    echo "Cancelled"
-		    exit
-		    ;;
-	    *)
-		    echo "ERROR: Invalid action"
-		    exit 1
-		    ;;
-    esac
-
-    echo -n "Setting new MAC (${mac_new})... "
-    if [ "${state}" == "up" ]; then
-	    ip link set dev wlan0 down
-    fi
-    ip link set dev wlan0 address "${mac_new}"
-    if [ "${state}" == "up" ]; then
-	    ip link set dev wlan0 up
-    fi
-
-    mac_new_real=$(get_mac_cur)
-    if [ "${mac_new_real}" == "${mac_new}" ]; then
-	    echo "Done"
-    elif [ "${mac_new_real}" == "${mac_cur}" ]; then
-	    echo "ERROR: Failed to change MAC address."
-    else
-	    echo "Something wacky happened."
-    fi
-    echo "Current MAC: ${mac_new_real}"
+    unblock_devmode
 
 elif [[ "$user_choice" = "7" ]]; then
-   curl -LOk mrchromebox.tech/firmware-util.sh
-   clear
-   sudo bash firmware-util.sh
+	fre_destroyer
 
 elif [[ "$user_choice" = "8" ]]; then
-    clear
-    echo "Are you sure you want to dump the current system BIOS/Firmware?"
-    echo "Nothing bad can come from this, it will just make the bios.bin file"
-    echo "in the directory you ran this script in."
-    echo ""
-    read -p "Type DoIt to confirm: " doit
-
-    if [[ "$doit" = "DoIt" ]]; then
-        clear
-        echo "Dumping system BIOS/Firmware..."
-        BIOSDump
-        echo "Dumped successfully."
-    else
-        clear
-        echo "User didn't want to do it..."
-    fi
+	reset_system
 
 elif [[ "$user_choice" = "9" ]]; then
-    # Customize the script's output appearance
-    clear
-    echo -e "\e[1mSystem Information\e[0m"
-    echo "---------------------"
+	systeminfo
 
-    # Gather and display system information
-    print_header "General"
-    get_cpu_usage
-    get_memory_usage
-    get_disk_space
+elif [[ "$user_choice" = "10" ]]; then
+	# All credits go to MrChromebox for this utility.
+    firmwareutil
 
-    print_header "System Load"
-    get_load_average
+elif [[ "$user_choice" = "11" ]]; then
+	# All credits go to Mercury Workshop for this utility.
+    macaddressrandomizer
 
-    print_header "Process Count"
-    get_process_count
-
-    print_header "System Uptime"
-    get_system_uptime
-
-    print_header "Swap Usage"
-    get_swap_usage
-
-    echo
-
-    # Press anykey to continue
-    read -p "Press anykey to continue: "
+elif [[ "$user_choice" = "12" ]]; then
+	dumpbios
 
 elif [[ "$user_choice" =~ [Rr] ]]; then
 	reboot
@@ -828,76 +384,4 @@ elif [[ "$user_choice" =~ [Rr] ]]; then
 elif [[ "$user_choice" =~ [Qq] ]]; then
 	exit 0
 
-elif [[ "$user_choice" = "10" ]]; then
-	clear
-	echo_red "ARE YOU SURE YOU WANT TO DO THIS?"
-	echo "THIS WILL BREAK FORCE RE-ENROLLMENT"
-	echo "MEANING YOU CAN NEVER RE-ENROLL EVER AGAIN."
-	echo "BY TYPING DoIt, YOU ACKNOWLEDGE THAT THE CREATOR"
-	echo "IS NOT RESPONSIBLE FOR ANY DAMAGE/HARM THAT COMES"
-	echo "FROM THIS SCRIPT."
-	read -p "Type DoIt to accept and continue: " doit
-
-	if [[ "$doit" = "DoIt" ]]; then
-		clear
-		echo_red "Breaking force re-enrollment..."
-		vpd -i "RW_VPD" -s "check_enrollment"="0" &> /dev/null
-  		vpd -i "RW_VPD" -s "block_devmode"="0" &> /dev/null
-  		vpd -d "stable_device_secret_DO_NOT_SHARE" &> /dev/null
-  		dump_vpd_log --force &> /dev/null
-  		crossystem clear_tpm_owner_request=1
-		clear
-		echo "Done!"
-		sleep 3
-		clear
-		echo "Rebooting. Please wait."
-		sleep 5
-		reboot
-	else
-		echo "Exiting..."
-		exit 0
-	fi
-
-elif [[ "$user_choice" = "11" ]]; then
-	clear
-	echo "Are you sure you want to do this?"
-	read -p "Type DoIt to confirm: " doit
-
-	if [[ "$doit" = "DoIt" ]]; then
-		clear
-		echo "Unblocking developer mode..."
-		vpd -i RW_VPD -s block_devmode=0
-		crossystem block_devmode=0
-  		flashrom -r bios.bin &> /dev/null
-  		gbb_utility --set --flags=$set_gbbchoice bios.bin &> /dev/null
-  		flashrom -i GBB -w bios.bin &> /dev/null
-  		rm bios.bin
-		clear
-		echo "Success!"
-	else
-		echo "Exiting..."
-		exit 0
-	fi
-
-elif [[ "$user_choice" = "2" ]]; then
-	clear
-	echo "Are you sure you want to re-enable autoupdates?"
-	read -p "Type DoIt to confirm: " doit
-
-	if [[ "$doit" = "DoIt" ]]; then
-		clear
-		echo "Re-Enabling autoupdates..."
-		mv /usr/sbin/update_enginefake /usr/sbin/update_engine
-		mv /usr/sbin/update_engine_clientfake /usr/sbin/update_engine_client
-		echo "Complete!"
-
-	else
-		clear
-		echo "Exiting..."
-	fi
-
-elif [[ "$user_choice" = "12" ]]; then
-	edit_crossystem
-elif [[ "$user_choice" = "13" ]]; then
-	reset_system
 fi
